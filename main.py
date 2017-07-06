337d2212cc34d8195b6158170a6c4a02d39b2d6e
Winston Lu <mobcraft120@gmail.com>
2017-07-06 10:03:29 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,44 +1,49 @@
 import random
+import tkinter as tk
+from tkinter import *
+import os
+file = open('sentenceData.txt', 'a+')
+file.close()
 file = open('sentenceData.txt', 'r+')
-helpCommand = (open('help.txt', 'r'))
-debugMode = False
 if file.read() == "":
     print("Sentence Data is empty...loading default phrases")
     file.write("Hello!,Howdy!")
     file.close()
     file = open('sentenceData.txt', 'r+')
+    phrases = file.read().split(',')
 else:
     file = open('sentenceData.txt', 'r+')
     phrases = file.read().split(',')
+root = Tk()
+aiOutput = "asdfsadf"
+T = Label(root, height=1, text=aiOutput)
+T.grid(row=0)
+userEntry = Entry(root, width=50)
+userEntry.grid(row=1)
+helpCommand = (open('help.txt', 'r'))
+debugMode = False
 talk = True
-while talk == True:
-    UserInput = input(phrases[random.randint(0, len(phrases)-1)] + " ")
-    if(UserInput[0] == "/"):
-        if(UserInput == "/help"):
-            print(helpCommand.read())
-        if(UserInput == "/exit"):
-            print("Closing...")
-            for i in range (len(phrases)):
-                file.write(phrases[i])
-                if i < len(phrases)-1:
-                    file.write(",")
-            file.close()
-            talk = False
-        elif(UserInput == "/debug"):
-            print("Syntax: /debug [value]")
-        elif(UserInput == "/debug 1"):
-            print("Starting debug...hold on.")
-            debugMode = True
-        elif(UserInput == "debug 0"):
-            print("Closing debug...hold on.")
-            debugMode = False
-        if(debugMode == True):
-            if(UserInput == "/phrases"):
-                print(phrases)
-            elif(UserInput == "/len"):
-                print(len(phrases))
-            elif(UserInput == "/clear"):    #   Doesn't work
-                phrases = []
+UserInput = ""
+aiOutput = phrases[random.randint(0, len(phrases)-1)] + " "
+def buttonCommand():
+    global talk
+    global aiOutput
+    global userEntry
+    global UserInput
+    global phrases
+    global debugMode
+    global T
+    if talk == True:
+        UserInput = userEntry.get()
+        userEntry.delete(0, 'end')
+        print(aiOutput)
+        T.text = aiOutput
+        aiOutput = phrases[random.randint(0, len(phrases)-1)] + " "
+        T = Label(root, height=1, text=aiOutput)
+        if(UserInput[0] == "/"):
+            if(UserInput == "/help"):
+                print(helpCommand.read())
+            elif(UserInput == "/exit"):
                 print("Closing...")
                 for i in range (len(phrases)):
                     file.write(phrases[i])
@@ -46,8 +51,29 @@ while talk == True:
                         file.write(",")
                 file.close()
                 talk = False
+            elif(UserInput == "/debug"):
+                print("Syntax: /debug [value]")
+            elif(UserInput == "/debug 1"):
+                print("Starting debug...hold on.")
+                debugMode = True
+            elif(UserInput == "/debug 0"):
+                print("Closing debug...hold on.")
+                debugMode = False
+            elif(debugMode == True):
+                if(UserInput == "/phrases"):
+                    print(phrases)
+                elif(UserInput == "/len"):
+                    print(len(phrases))
+                elif(UserInput == "/clear"):
+                    file.close()
+                    os.remove('sentenceData.txt')
+                    talk = False
         
+            else:
+                print("Enter a valid command!")
         else:
-            print("Enter a valid command!")
-    else:
-        phrases.append(UserInput)
+            phrases.append(UserInput)
+
+userConfirm = Button(root, text="Talk to the AI!", command=buttonCommand)
+userConfirm.grid(row=1, column=1)
+mainloop()
