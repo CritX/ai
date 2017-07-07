e79956b3796bc5305c5bbb711829f9829deefe31
Winston Lu <mobcraft120@gmail.com>
2017-07-07 10:00:10 -0500

    1.0.3

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
-import random
-import tkinter as tk
+import random   # random ints
+import tkinter as tk    # the scren
 from tkinter import *
-import os
-file = open('sentenceData.txt', 'a+')
+import os   # deleting files when doing /clear
+
+#
+#   AI (in Python)
+#   Created by NotJustCringe
+#   Version 1.0
+#
+
+file = open('sentenceData.txt', 'a+')   # Makes sure the file is created if it isn't
 log = []
 logFile = open('log.txt', 'a+')
 logFile.close()
@@ -13,7 +20,7 @@ if file.read() == "":
     print("Sentence Data is empty...loading default phrases")
     file.write("Hello!,Howdy!")
     file.close()
-    logFile.write("Hello!,Howdy!")
+    logFile.write("Hello!,Howdy!")  # This is unnecessary, I just don't want to change it :P
     logFile.close()
     logFile = open('log.txt', 'r+')
     log = logFile.read().split(',')
@@ -24,7 +31,7 @@ else:
     file = open('sentenceData.txt', 'r+')
     phrases = file.read().split(',')
     log = logFile.read().split(',')
-root = Tk()
+root = Tk() # Creates the screen
 root.configure(background='#7DEE9F')
 logo = PhotoImage(file="logo.png").subsample(2, 2)
 placeholderLabel = Label(root, image=logo, bg = '#7DEE9F').grid(row=0)
@@ -38,7 +45,8 @@ debugMode = False
 talk = True
 UserInput = ""
 aiOutput.set(phrases[random.randint(0, len(phrases)-1)])
-def buttonCommand():
+def buttonCommand():    # The reason this is all in a command is so I can use the command property in the button
+    """The main button command, this makes the AI 'respond' to the user"""
     global talk
     global aiOutput
     global userEntry
@@ -56,7 +64,11 @@ def buttonCommand():
         
         
         smart()
-        if(UserInput[0] == "/"):
+        if(UserInput[0] == "/"):    # Makes sure commands or command typos don't affect the log
+            #
+            # NOTE:
+            # All commands are printed to the console, NOT the bot
+            #
             if(UserInput == "/help"):
                 print(helpCommand.read())
             elif(UserInput == "/exit"):
@@ -83,7 +95,7 @@ def buttonCommand():
                 logFile.close()
                 talk = False
                 root.destroy()
-            elif(UserInput == "/debug"):
+            elif(UserInput == "/debug"):    # Just makes sure users can't type in any debugging commands on default
                 print("Syntax: /debug [value]")
             elif(UserInput == "/debug 1"):
                 print("Starting debug...hold on.")
@@ -96,26 +108,31 @@ def buttonCommand():
                     print(phrases)
                 elif(UserInput == "/len"):
                     print(len(phrases))
-                elif(UserInput == "/clear"):
+                elif(UserInput == "/clear"):    # Closes similarly to /exit but doesn't write
                     file.close()
                     os.remove('sentenceData.txt')
                     logFile.close()
                     os.remove('log.txt')
                     talk = False
                     root.destroy()
+                elif(UserInput == "/log"):
+                    print(log)
+                elif(UserInput == "/len log"):
+                    print(len(log))
             else:
                 print("Enter a valid command!")
         else:
-           log.append(aiOutput.get())
+           log.append(aiOutput.get())   # Appends what the AI says and what the user says
            phrases.append(UserInput)
-def smart():
+def smart():    # This function works similarly to Cleverbot, taking user input and finding how it correlates to what the AI says
+    """The 'smartness and responsive' algorithm for the AI"""
     global phrases
     global aiOutput
     global log
     global UserInput
     done = 0
     choose = []
-    if(random.randint(1,9)<8):
+    if(random.randint(1,9)<8):  # Don't want the bot to always give the 'correct' response to what has been said to make it more realistic
         for i in range (len(log)):
             if (done == 0):
                 if(UserInput == log[i]):
@@ -126,4 +143,4 @@ def smart():
                 
 userConfirm = Button(root, text="Talk to the AI!", command=buttonCommand)
 userConfirm.grid(row=2,column=1)
-root.mainloop()
+root.mainloop() # Starts main loop
